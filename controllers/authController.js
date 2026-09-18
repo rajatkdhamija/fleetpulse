a0554fd2e93fc1d0f17cf9b8fc3c34505f498400
Rajat Dhamija <rajatkdhamija@gmail.com>
2026-09-19 02:22:20 +0530

    Indentation fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,6 @@ import { db } from "../src/prisma/db.js";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-
 export const register = async (req, res, next) => {
     try {
         const { email, password } = req.body || {};
